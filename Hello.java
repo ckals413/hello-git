570e7d42ad2256d075df62d9a6749c5130a9cd20
Chamin Im <ckals413@naver.com>
2023-11-08 19:10:49 +0000

    added license to Hello.java

--- Hello.java
+++ Hello.java
@@ -1,9 +1,12 @@
 //Hello.java
+//수정1
+
 
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello, world from GIT!");
 		System.out.println("Changes after First commit!");
+
 	}
 }
 
